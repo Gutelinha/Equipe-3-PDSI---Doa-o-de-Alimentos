fb67cbafb3a0280e5100b346afb98f0eaa226f8c
Livia <liviafausto03@gmail.com>
2024-11-12 22:17:55 -0300

    feat: estrutura básica de cadastro de produto

--- back-end/src/product/product.controller.ts
+++ back-end/src/product/product.controller.ts
@@ -1,14 +1,15 @@
-import { Produto } from '@prisma/client';
 import { ProductService } from './product.service';
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { ProductDto } from './dto';
 
 @Controller('products')
 export class ProductController {
     constructor(private readonly productService: ProductService) {}
 
     @Post()
-    saveProduct(@Body() product: Produto) {
-        return this.productService.saveProduct(product);
+    saveProduct(@Body() productDto: ProductDto) {
+        console.log('Creating new product:', productDto);
+        return this.productService.saveProduct(productDto);
     }
 
 }
--- back-end/src/product/product.service.ts
+++ back-end/src/product/product.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from "@nestjs/common";
-import { Produto } from "@prisma/client";
+import { ProductDto } from "./dto";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class ProductService {
+    constructor(private prisma: PrismaService) {}
 
-    saveProduct(product: Produto) {
-        return "Saved product";
+    async saveProduct(productDto: ProductDto) {
+        const product = await this.prisma.produto.create({
+            data: {
+                codigo_barras: productDto.barcode,
+                nome: productDto.name,
+                marca: productDto.brand,
+                tipo: productDto.type,
+                unidade_volume: productDto.volumeUnit
+            }
+        });
+        return product;
     }
     
 }
