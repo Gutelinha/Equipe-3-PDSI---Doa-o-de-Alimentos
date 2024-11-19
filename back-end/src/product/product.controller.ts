dff841677da93c34d75943258e5aed4cc0ec8d5b
Lucas Loureiro Rodrigues <lucas@loureiro.tech>
2024-11-18 22:36:28 -0300

    test: comment para dar trigger no ci

--- back-end/src/product/product.controller.ts
+++ back-end/src/product/product.controller.ts
@@ -1,7 +1,7 @@
 import { ProductService } from './product.service';
 import { Body, Controller, Post } from "@nestjs/common";
 import { ProductDto } from './dto';
-
+//
 @Controller('products')
 export class ProductController {
     constructor(private readonly productService: ProductService) {}
