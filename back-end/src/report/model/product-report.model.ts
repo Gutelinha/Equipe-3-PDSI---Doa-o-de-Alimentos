e472a7e79db9e1f4edd21b958da222fe92c5f16d
Livia <liviafausto03@gmail.com>
2024-12-03 22:05:39 -0300

    feat: implementação do cálculo do volume total

--- back-end/src/report/model/product-report.model.ts
+++ back-end/src/report/model/product-report.model.ts
@@ -1,3 +1,5 @@
+import { extractVolumeNumberFromString, extractVolumeUnitFromString, volumeToString } from "src/utils/volume-converter";
+
 export class ProductReportModel {
     name: string;
     volume_unit: string;
@@ -8,5 +10,17 @@ export class ProductReportModel {
         this.name = name;
         this.volume_unit = volume_unit;
         this.quantity = Number(quantity);
+        this.calculateTotalVolume();
+    }
+
+    private calculateTotalVolume(): void {
+        const unit: string = extractVolumeUnitFromString(this.volume_unit);
+        const volume: number = extractVolumeNumberFromString(this.volume_unit);
+        let total: number = 0;
+
+        if(volume !== null)
+            total = volume * this.quantity;
+
+        this.total_volume = volumeToString(total, unit);
     }
 }
